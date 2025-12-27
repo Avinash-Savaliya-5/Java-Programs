cbba3d6b8099e4f3d9fa9c1c87ca371045591bf7
Avinash-Savaliya-5 <absavaliya2005@gmail.com>
2025-12-27 19:34:26 +0530

    Java

---
+++ Pr_20_Ugly_number_check.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Pr_20_Ugly_number_check{
+    public boolean uglyCheck(int num){
+        if(num < 1){
+            return false;
+        }
+        while(num%2==0){
+            num=num/2;
+        }
+        while(num%3==0){
+            num=num/3;
+        }
+        while(num%5==0){
+            num=num/5;
+        }
+        if(num==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Pr_20_Ugly_number_check obj = new Pr_20_Ugly_number_check();
+
+        boolean ans = obj.uglyCheck(num);
+        if(ans == true){
+            System.out.println(num + " is ugly number");
+        }
+        else{
+            System.out.println(num + " is not ugly number");
+        }
+
+    }
+}
